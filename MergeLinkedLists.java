6eca3687ad620e301b623747ea5b4839e39a4c4e
shraddhamanipal <162777444+shraddhamanipal@users.noreply.github.com>
2024-03-11 12:54:27 +0530

    Add files via upload

---
+++ MergeLinkedLists.java
@@ -0,0 +1,64 @@
+class MergeLinkedLists {
+
+    public static void main(String[] args) {
+        ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(3, null)));
+        ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(5, null)));
+        ListNode result = mergeTwoLists(list1, list2);
+        System.out.println(result.toString());
+    }
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null) {
+            return list2;
+        }
+        if(list2 == null) {
+            return list1;
+        }
+        ListNode head;
+        if (list1.val <= list2.val) {
+            head = list1;
+            list1 = list1.next;
+        } else {
+            head = list2;
+            list2 = list2.next;
+        }
+        ListNode cur = head;
+        while(list1 != null && list2 != null) {
+            if(list1.val <= list2.val) {
+                cur.next = list1;
+                list1 = list1.next;
+            } else {
+                cur.next = list2;
+                list2 = list2.next;
+            }
+            cur = cur.next;
+        }
+        if(list1!= null) {
+            cur.next = list1;
+        } else {
+            cur.next = list2;
+        }
+        return head;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) {
+        this.val = val;
+    }
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                ", next=" + next +
+                '}';
+    }
+}
