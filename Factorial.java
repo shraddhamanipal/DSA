34d5e25a63b89fd32ee2c43386d0f2393f0c5228
shraddhamanipal <162777444+shraddhamanipal@users.noreply.github.com>
2024-03-12 23:06:18 +0530

    Recursive & iterative factorial solution

---
+++ Factorial.java
@@ -0,0 +1,22 @@
+class Factorial {
+
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println(factorialRecur(n));
+        System.out.println(factorialIter(n));
+    }
+    private static int factorialRecur(int n) {
+        if(n == 0 || n == 1) {
+            return 1;
+        }
+        return n * factorialRecur(n-1);
+    }
+
+    private static int factorialIter(int n) {
+        int res = 1;
+        for(int i = 1; i <=n; i++) {
+            res = res * i;
+        }
+        return res;
+    }
+}
