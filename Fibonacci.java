dba1acf641a07e5e4621b7542d35d1e815a74d3a
shraddhamanipal <162777444+shraddhamanipal@users.noreply.github.com>
2024-03-12 23:58:28 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,22 +1,12 @@
 class Fibonacci {
-    private static int sum = 0;
     public static void main(String[] args) {
-        int n = 10, res;
-        if(n == 0) {
-            res = 0;
-        } else if(n == 1) {
-            res = 1;
-        } else {
-            res = fibonacciRecur(0, 1, n-1);
-        }
-        System.out.println(res);
+        int n = 8;
+        System.out.println(fibonacciRecur(n));
     }
-    private static int fibonacciRecur(int n1, int n2, int n) {
-        if(n>0) {
-            sum = n1 + n2;
-            n--;
-            return fibonacciRecur(n2, sum, n);
+    private static int fibonacciRecur(int n) {
+        if(n < 2) {
+            return n;
         }
-        return sum;
+        return fibonacciRecur(n-1) + fibonacciRecur(n-2);
     }
 }
